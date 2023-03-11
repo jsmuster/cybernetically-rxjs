78ce903d8dfe69060c86081f0c301d5c4cec9275
Arseniy Tomkevich <arseniytomkevich@Arseniys-MacBook-Pro.local>
2023-03-11 13:23:00 -0500

    added a few more methods

--- Observable.js
+++ Observable.js
@@ -1,12 +1,19 @@
-const Observable = class
+const RxObservable = class
 {
   #executor
-  constructor(executor = null)
+  #destroyer
+  constructor(executor = null, destroyer = null)
   {
     if(executor != null && typeof(executor) == 'function')
     {
       // save the method into a private variable
       this.#executor = executor;
+    }
+
+    if(destroyer != null && typeof(destroyer) == 'function')
+    {
+      // save the method into a private variable
+      this.#destroyer = destroyer;
     }
   }
   subscribe(subscriber, error, complete)
@@ -41,11 +48,15 @@ const Observable = class
       }
       else if(typeof(subscriber) == 'object' && subscriber.next != null)
       {
-        sub = {next: subscriber.next};
+        sub = {next: (val) => {
+          subscriber.next.call(subscriber, val);
+        }};
         
         if(subscriber.error != null)
         {
-          sub.error = subscriber.error;
+          sub.error = () => {
+            subscriber.error.call(subscriber);
+          };
         }
         else
         {
@@ -55,7 +66,9 @@ const Observable = class
         
         if(subscriber.complete != null)
         {
-          sub.complete = subscriber.complete;
+          sub.complete = () => {
+            subscriber.complete.call(subscriber);
+          };
         }
         else
         {
@@ -69,12 +82,117 @@ const Observable = class
         this.#executor(sub);
       }
       catch(e) {
+      }
 
+      if(this.#destroyer != null)
+      {
+        let subscription = new RxSubscription(this.#destroyer);
+
+        return subscription;
+      }
+    }
+
+  }
+  pipe()
+  {
+    
+  }
+};
+
+const RxSubscription = class
+{
+  #executor
+  constructor(executor = null)
+  {
+    if(executor != null && typeof(executor) == 'function')
+    {
+      // save the method into a private variable, to triger later in unsubscribe
+      this.#executor = executor;
+    }
+  }
+  unsubscribe()
+  {
+    if(this.#executor != null)
+    {
+      try
+      {
+        this.#executor();
+      }
+      catch(e)
+      {
+        console.log("Error unsubscribing ", e.stack != null ? e.stack : e);
       }
     }
   }
 };
 
-const rxjs = {Observable:{create:(subscriber) => {
-  return new Observable(subscriber);
+const rxjs = {Observable:{create:(subscriber, destroyer) => {
+  return new RxObservable(subscriber, destroyer);
 }}};
+
+/* of Operator */
+rxjs.of = (...args) => {
+
+  return rxjs.Observable.create((subscriber) => {
+
+    args.forEach((val) => {
+      subscriber.next(val);
+    });
+    
+    subscriber.complete();
+  });
+
+};
+
+/* interval Operator */
+rxjs.interval = (time) => {
+
+  // interval id to be used with subscription to unsubscribe
+  let iid;
+
+  // number that gets incremented every time
+  let num = 0;
+
+  // create a subscription object that is used to unsubscribe and clear the interval
+  return rxjs.Observable.create((subscriber) => {
+
+    iid = setInterval( () => {
+
+      subscriber.next(num++);
+
+    }, time);
+
+  }, () => {
+    clearInterval(iid);
+  });
+
+};
+
+/* range Operator */
+rxjs.range = (from, count) => {
+
+  return rxjs.Observable.create((subscriber) => {
+    
+    for(let i = from, l = from + count; i < l; i++)
+    {
+      subscriber.next(i);
+    }
+    
+    subscriber.complete();
+  });
+
+};
+
+/* rxjs Operators */
+rxjs.operators = {};
+
+rxjs.operators.filter = () => {
+  
+};
+
+rxjs.operators.map = () => {
+  
+};
+
+
+
